ecafb60f8bb9df58550a341de9ce82d644e401c2
francoisfagan <francois.fagan@gmail.com>
2018-04-07 10:20:32 -0400

    Linear, Relu, Hardtanh and arctan for ISGD are all working on the mnist dataset

--- ISGD_fns.py
+++ ISGD_fns.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import math
 from torch.autograd.function import once_differentiable
+import numpy as np
+from functools import reduce
 
 
 def calc_logit(input, weight, bias=None):
@@ -83,6 +85,13 @@ def calc_weigh_bias_grad(weight, mu, lr, a, d, input, z_norm, bias):
     grad_weight = weight * mu / (1.0 + lr * mu) - (a * d).t().mm(input) / z_norm ** 2
     grad_bias = bias * mu / (1.0 + lr * mu) - (a * d).squeeze() / z_norm ** 2
     return grad_weight, grad_bias
+
+
+def real_root_closest_to_zero(coeff):
+    roots = np.roots(coeff)
+    real_roots = [root.real for root in roots if root.imag == 0]
+    root_closest_to_zero = reduce((lambda x, y: x if (abs(x) < abs(y)) else y), real_roots)
+    return root_closest_to_zero
 
 
 class HyperparameterClass:
@@ -280,12 +289,18 @@ class IsgdReluFunction(torch.autograd.Function):
                                                                                        grad_output, lr, mu)
 
         # Calculate a
+        # Calculate conditions for a
         cond1 = ((s == +1) * (c <= 0)).type(torch.FloatTensor)
         cond2 = ((s == +1) * (c > 0) * (c <= (lr * d ** 2))).type(torch.FloatTensor)
         cond3 = ((s == +1) * (c > (lr * d ** 2))).type(torch.FloatTensor)
         cond4 = ((s == -1) * (c <= -(lr * d ** 2) / 2.0)).type(torch.FloatTensor)
         cond5 = ((s == -1) * (c > -(lr * d ** 2) / 2.0)).type(torch.FloatTensor)
 
+        # Check that exactly one condition satisfied for each node
+        cond_sum = (cond1 + cond2 + cond3 + cond4 + cond5)
+        assert (torch.mean((cond_sum == 1).type(torch.FloatTensor)) == 1.0)
+
+        # Calculate a
         a = (0.0 * cond1
              - (c / (lr * d)) * cond2
              - d * cond3
@@ -360,25 +375,133 @@ class IsgdHardTanhFunction(torch.autograd.Function):
         input, weight, bias, output, logit, s, z_norm, d, c = calc_backwards_variables(ctx.saved_tensors, ctx.logit,
                                                                                        grad_output, lr, mu)
 
-        # # Calculate a
-        # a = None  # THIS NEEDS TO BE FILLED IN
-        #
-        # # Calculate input gradient
-        # grad_input = None  # THIS NEEDS TO BE FILLED IN
-        #
-        # # Calculate grad_weight, grad_bias and return all gradients
-        # grad_weight, grad_bias = calc_weigh_bias_grad(weight, mu, lr, a, d, input, z_norm, bias)
+        # Calculate a
+        sc = s * c
+        cond1 = ((sc <= -1)
+                 + (sc >= (1 + lr * d ** 2 / 2))).type(torch.FloatTensor)
+        cond2 = ((sc >= (lr * d ** 2 - 1)) * (sc <= 1)
+                 + ((sc >= torch.clamp(lr * d ** 2 / 2 - 1, min=1))
+                    * (sc <= (1 + lr * d ** 2 / 2)))).type(torch.FloatTensor)
+        cond3 = ((sc >= -1) * (sc <= torch.clamp(lr * d ** 2 - 1, max=1))
+                 + (sc >= 1) * (sc <= (lr * d ** 2 / 2 - 1))).type(torch.FloatTensor)
 
-        # Find all nodes where the output is greater than or equal to 0
+        # Check that at least one condition satisfied for each node
+        # (it is possible for multiple conditions to be satisfied)
+        cond_sum = (cond1 + cond2 + cond3)
+        assert (torch.mean((cond_sum >= 1).type(torch.FloatTensor)) == 1.0)
+
+        # If multiple conditions satisfied then normalize
+        cond1 = cond1 / cond_sum
+        cond2 = cond2 / cond_sum
+        cond2 = cond2 / cond_sum
+
+        # Calculate a
+        a = (0.0 * cond1
+             - s * d * cond2
+             - (1 + sc) / (lr * d) * cond3
+             )
+
+        # a might contain Nan values if d = 0 at certain elements due to diving by d in (c / (lr * d)) * cond2
+        # The operation below sets all Nans to zero
+        # This is the appropriate value for ISGD
+        a[a != a] = 0
+
+        # Calculate grad_weight, grad_bias and return all gradients
+        grad_weight, grad_bias = calc_weigh_bias_grad(weight, mu, lr, a, d, input, z_norm, bias)
+
+        # Calculate input gradient
         non_clamped = ((output > -1) * (output < 1)).type(torch.FloatTensor)  # [1 x m]
-
-        # Mask the back-propagated gradient to zero out elements where the output is zero.
         grad_output_masked = non_clamped * grad_output  # [1 x m]
+        grad_input = grad_output_masked.mm(weight)  # [1 x n]
 
         # Calculate gradients
-        grad_input = grad_output_masked.mm(weight)  # [1 x n]
-        grad_weight = grad_output_masked.t().mm(input)  # [m x n]
-        grad_bias = grad_output_masked.sum(0).squeeze(0)  # [m]
+        # grad_weight = grad_output_masked.t().mm(input)  # [m x n]
+        # grad_bias = grad_output_masked.sum(0).squeeze(0)  # [m]
+        return grad_input, grad_weight, grad_bias
+
+
+class IsgdArctanFunction(torch.autograd.Function):
+
+    # Note that both forward and backward are @staticmethods
+    @staticmethod
+    # bias is an optional argument
+    def forward(ctx, input, weight, bias=None):
+        """
+        Calculate forward-propagation and store variables need for back-propagation
+
+        Args:
+            ctx:
+            input: [1 x n]      Input vector
+            weight: [m x n]     Weight matrix
+            bias: [m]           Bias vector
+
+        Returns:
+            output [1 x m]     Input to the next layer = logit put through the non-linear activation function
+        """
+
+        # Calculate logit [1 x m], where logit = input.mm(weight.t()) + bias
+        logit = calc_logit(input, weight, bias)
+
+        # Non-linear activation function
+        output = torch.atan(logit)  # THIS NEEDS TO BE FILLED IN
+
+        # Save context for back-propagation
+        ctx.save_for_backward(input, weight, bias, output)
+        ctx.logit = logit
+
+        return output
+
+    @staticmethod
+    @once_differentiable
+    def backward(ctx, grad_output):
+        """
+        Calculate back-propagation gradients for input, weight and bias
+
+        Args:
+            ctx:                    Context save from the forward-propagation
+            grad_output: [1 x m]    The gradient that has been back-propagated to this layer
+
+        Returns:
+            grad_input: [1 x n]     Gradient of the input vector
+            grad_weight: [m x n]    Gradient of the weight matrix
+            grad_bias: [m]          Gradient of the bias vector
+        """
+
+        # Retrieve parameters required for the update
+        lr, mu = HyperparameterClass.lr, HyperparameterClass.mu
+        input, weight, bias, output, logit, s, z_norm, d, c = calc_backwards_variables(ctx.saved_tensors, ctx.logit,
+                                                                                       grad_output, lr, mu)
+
+        # Calculate a
+        # # First method using algebraic root finding
+        # # It is slow since it needs to move from pytorch to numpy and back
+        # coeff = np.array([((lr * d) ** 2).numpy()[0],
+        #                   (2 * lr * d * c).numpy()[0],
+        #                   (c ** 2 + 1).numpy()[0],
+        #                   (s * d).numpy()[0]])
+        #
+        # root_closest_to_zero = np.apply_along_axis(real_root_closest_to_zero, 0, coeff)
+        # a = torch.from_numpy(root_closest_to_zero).unsqueeze(1).t().type(torch.FloatTensor)
+
+        # Second method is iterative
+        # It stays in pytorch, so is faster
+        a = d * 0
+        a_diff = 1  # Norm difference between previous and current a values
+        iter_count = 0  # Count of number of a iterations
+        while a_diff < 1e-10:
+            a_new = - s * d / (1.0 + (lr * d * a + c) ** 2)
+            a_diff = torch.norm(a - a_new)
+            a = a_new
+            iter_count += 1
+            assert (iter_count < 20), 'Arctan update has failed to converge'
+
+        # Calculate grad_weight, grad_bias and return all gradients
+        grad_weight, grad_bias = calc_weigh_bias_grad(weight, mu, lr, a, d, input, z_norm, bias)
+
+        # Calculate input gradient
+        grad_output_scaled = grad_output / (1 + logit ** 2)  # [1 x m]
+        grad_input = grad_output_scaled.mm(weight)  # [1 x n]
+
         return grad_input, grad_weight, grad_bias
 
 
@@ -564,4 +687,50 @@ class IsgdHardTanh(nn.Module):
             output: [1 x n]     Output features for subsequent layer
         """
         output = IsgdHardTanhFunction.apply(input, self.weight, self.bias)
+        return output
+
+
+class IsgdArctan(nn.Module):
+    def __init__(self, input_features, output_features, bias=True):
+        """
+        Initialize the module by storing the input and outputfeatures
+        as well as creating and initializing the parameters
+
+        Args:
+            input_features: [1 x n]     Input features from previous layer
+            output_features: [1 x m]    Output features for subsequent layer
+            bias:
+        """
+        super(IsgdArctan, self).__init__()
+
+        # Store features
+        self.input_features = input_features
+        self.output_features = output_features
+
+        # Create parameters
+        self.weight = nn.Parameter(torch.Tensor(output_features, input_features))
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(output_features))
+        else:
+            self.register_parameter('bias', None)
+
+        # Initialize parameters
+        # Not a very smart way to initialize weights
+        self.weight.data.uniform_(-0.1, 0.1)
+        if bias is not None:
+            self.bias.data.uniform_(-0.1, 0.1)
+
+    def forward(self, input):
+        """
+        Set forward and backward-propagation functions.
+        Even though this function is called "forward",
+        the class which it calls defines both forward and backward-propagations
+
+        Args:
+            input: [1 x m]      Input features from previous layer
+
+        Returns:
+            output: [1 x n]     Output features for subsequent layer
+        """
+        output = IsgdArctanFunction.apply(input, self.weight, self.bias)
         return output
--- mnist_example.py
+++ mnist_example.py
@@ -47,7 +47,7 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = IsgdHardTanh(320, 50)  #nn.Linear(320, 50)  #
+        self.fc1 = IsgdArctan(320, 50)  #nn.Linear(320, 50)  #
         self.fc2 = IsgdRelu(50, 10)  #nn.Linear(50, 10)  #
 
     def forward(self, x):
